b0721d914a069412cb996140d850b0dbdc9a8785
kovavka <kovavka@gmail.com>
2018-01-13 00:36:46 +0500

    Damain

---
+++ SUBD/Domain/DataBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class DataBase: Entity
+    {
+        public virtual string Name { get; set; }
+        
+        public virtual List<Table> Tables { get; set; }
+    }
+}
---
+++ SUBD/Domain/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public interface IEntity
+    {
+        int Id { get; set; }
+    }
+    
+    public class Entity: IEntity
+    {
+        public virtual int Id { get; set; }
+    }
+}
