0333d773b914575362e70c8bdf7a70d14b8cd1e6
Сергеев Михаил Игоревич <ima1ta1r@yandex.ru>
2018-01-13 01:32:16 +0500

    Miwka 2 (#2)
    * Added entities, needs further work
    
    * Inheriting entities from Entity base class

--- SUBD/Domain/Entity.cs
+++ SUBD/Domain/Entity.cs
@@ -1,16 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Domain
+﻿namespace Domain
 {
-    public interface IEntity
-    {
-        int Id { get; set; }
-    }
-    
     public class Entity: IEntity
     {
         public virtual int Id { get; set; }
---
+++ SUBD/Domain/Relation.cs
@@ -0,0 +1,9 @@
+﻿namespace Domain
+{
+    public class Relation: Entity
+    {
+        public virtual int SourceTableId { get; set; }
+        
+        public virtual int DestinationTableId { get; set; }
+    }
+}
