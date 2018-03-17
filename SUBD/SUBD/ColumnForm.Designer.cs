7162b8ae73ed3fd91684777164cd03c37c50b741
gikap <kovavka@gmail.com>
2018-03-17 21:54:28 +0500

    +1

---
+++ SUBD/SUBD/ColumnForm.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace SUBD
+{
+    partial class ColumnForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.gridView = new SUBD.GridView();
+            this.save = new System.Windows.Forms.Button();
+            this.Column1 = new System.Windows.Forms.DataGridViewComboBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Базы данных";
+            // 
+            // gridView
+            // 
+            this.gridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.gridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.gridView.Location = new System.Drawing.Point(12, 30);
+            this.gridView.MaximumSize = new System.Drawing.Size(288, 273);
+            this.gridView.MinimumSize = new System.Drawing.Size(288, 273);
+            this.gridView.Name = "gridView";
+            this.gridView.Size = new System.Drawing.Size(288, 273);
+            this.gridView.TabIndex = 9;
+            // 
+            // save
+            // 
+            this.save.Location = new System.Drawing.Point(121, 323);
+            this.save.Name = "save";
+            this.save.Size = new System.Drawing.Size(75, 23);
+            this.save.TabIndex = 10;
+            this.save.Text = "Сохранить";
+            this.save.UseVisualStyleBackColor = true;
+            this.save.Click += new System.EventHandler(this.save_Click);
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Column1";
+            this.Column1.Name = "Column1";
+            // 
+            // DBForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(312, 358);
+            this.Controls.Add(this.save);
+            this.Controls.Add(this.gridView);
+            this.Controls.Add(this.label1);
+            this.MaximumSize = new System.Drawing.Size(328, 397);
+            this.MinimumSize = new System.Drawing.Size(328, 397);
+            this.Name = "DBForm";
+            ((System.ComponentModel.ISupportInitialize)(this.gridView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private GridView gridView;
+        private System.Windows.Forms.Button save;
+        private System.Windows.Forms.DataGridViewComboBoxColumn Column1;
+    }
+}
--- SUBD/SUBD/TableForm.cs
+++ SUBD/SUBD/TableForm.cs
@@ -14,6 +14,7 @@ namespace SUBD
     public partial class TableForm : Form
     {
         private readonly Server server;
+        private readonly List<BindingDataBase> bindingDataBases = new List<BindingDataBase>();
 
         public TableForm(Server server)
         {
@@ -23,19 +24,124 @@ namespace SUBD
             Text = "Data Source = " + server.Address;
             dbComboEdit.CanWrite = false;
 
+            dbComboEdit.SelectedIndexChanged += SelectedIndexChange;
+
             LoadDBs();
         }
-        
+
+        private int lastDatabaseId = -1;
+        private void SelectedIndexChange(object sended, EventArgs args)
+        {
+            if (Relations != null && Tables != null)
+            {
+                Last.Relations = Relations;
+                Last.Tables = Tables;
+                
+                Relations = new BindingList<Relation>(Current.Relations.ToList());
+                Tables = new BindingList<Table>(Current.Tables.ToList());
+
+                RecreateGrids();
+            }
+
+            lastDatabaseId = Database.Id;
+        }
+
+        private BindingDataBase Last
+        {
+            get { return bindingDataBases.Find(x => x.Database == lastDatabaseId); }
+        }
+
+        private BindingDataBase Current
+        {
+            get { return bindingDataBases.Find(x => x.Database == Database.Id); }
+        }
+
         private void LoadDBs()
         {
             //TODO: загрузить дб - List<Database>
             dbComboEdit.DataSource = server.Databases;
+            
+            foreach (var dataBase in server.Databases)
+            {
+                if (bindingDataBases.All(x => x.Database != dataBase.Id))
+                    bindingDataBases.Add(new BindingDataBase()
+                    {
+                        Database = dataBase.Id,
+                        Relations = new BindingList<Relation>(dataBase.Relations.ToList()),
+                        Tables = new BindingList<Table>(dataBase.Tables.ToList()),
+                    });
+            }
+
+            Relations = new BindingList<Relation>(Database.Relations.ToList());
+            Tables = new BindingList<Table>(Database.Tables.ToList());
+
+
+            RecreateGrids();
+
         }
+
+        private void RecreateGrids()
+        {
+            new GridViewCreator<Table>(gridViewTables, Tables)
+                .Column("Наименование", x => x.Name).NotNull();
+
+
+            new GridViewCreator<Relation>(gridViewRelations, Relations)
+                .Column("Наименование", x => x.Name).NotNull()
+                .Column("Атрибут 1", x => x.SourceColumn).ReadOnly()
+                .Column("Атрибут 2", x => x.DestinationColumn).ReadOnly();
+            
+        }
+
+        private Database Database
+        {
+            get {return (Database)dbComboEdit.Value; }
+        }
+
+        private BindingList<Relation> Relations;
+        private BindingList<Table> Tables;
 
         private void manage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
             new DBForm(server).ShowDialog();
             LoadDBs();
         }
+
+        private void save_Click(object sender, EventArgs e)
+        {
+            //TODO тут вот сохранить данные из BindingDataBase
+        }
+
+        private void gridViewRelations_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            var view = new RelationForm(server, Database, Current.Relations[e.RowIndex]);
+            if (view.ShowDialog() == DialogResult.OK)
+            {
+                Current.Relations[e.RowIndex] = view.GetRelation();
+                Relations = new BindingList<Relation>(Current.Relations.ToList());
+                RecreateGrids();
+            }
+        }
+
+        private void maskManage_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            new DBForm(server).ShowDialog();
+            LoadDBs();
+        }
+    }
+
+    class BindingDataBase
+    {
+        public int Database { get; set; }
+        public BindingList<Relation> Relations { get; set; }
+        public BindingList<Table> Tables { get; set; }
+    }
+
+    class BindingRelation
+    {
+
+        public virtual Table SourceTable { get; set; }
+
+        public virtual Table DestinationTable { get; set; }
     }
 }
